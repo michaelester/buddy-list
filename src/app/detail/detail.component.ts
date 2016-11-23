6eabb7308a1c88e8976abb4347f8dc858d8f7645
Michael Ester <michael.ester@netural.com>
2016-11-23 16:28:55 +0100

    reverted detail to promise

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SBStore, SBStory } from 'ng-storyblok';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
@@ -10,7 +9,7 @@ import { Observable } from 'rxjs';
 })
 export class DetailComponent implements OnInit {
 
-  private story: Observable<SBStory>;
+  private story: SBStory;
 
   constructor(private sbstore: SBStore, private route: ActivatedRoute) {
 
@@ -19,9 +18,7 @@ export class DetailComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.sbstore.story(params['id']).subscribe(s => {
-        console.log(s);
-      })
+      this.sbstore.findStory(params['id']).then(story => this.story = story);
     });
   }
 
